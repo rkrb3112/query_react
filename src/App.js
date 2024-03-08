ba1daa0a5cde613c7e4c066b6d17e80af77b2ec6
rkrb3112 <rkrb3112@gmail.com>
2024-03-08 13:25:28 +0530

    again

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import {Task} from "./Task";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () =>{
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+      completed: false,
+    }
+    setTodoList([...todoList, task]);
+  };
+
+  const deleteTask = (id) =>{
+    const newTodoList = todoList.filter((task) =>{
+      if(id === task.id) {
+        return false;
+      } else{
+        return true;
+      }
+    });
+
+    setTodoList(newTodoList);
+  };
+
+  const completeTask = (id) =>{
+    setTodoList(todoList.map((task) => task.id === id ? {...task, completed: true} : task));
+  };
+
+  return ( 
+    <div className='App'>
+      <div className='addTask'>
+        <input onChange={handleChange}/>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className='list'>
+        {todoList.map((task) =>{
+          return <Task taskName={task.taskName} id={task.id} deleteTask={deleteTask} completed={task.completed} completeTask={completeTask} />
+        })}
+      </div>
     </div>
   );
-}
+};
+
+
 
 export default App;
